8f70ef2a7e1dfbe16356aed21ea7258594099469
Douman <douman@gmx.se>
2016-12-19 09:42:10 +0300

    Add simple router

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,15 +1,53 @@
+import ReactDOM from 'react-dom';
 import React from 'react';
+import Timer from 'components/Timer.jsx';
+import Todo from 'components/Todo.jsx';
 
-export default class App extends React.Component {
+import { Router, IndexRoute, Link, Route, browserHistory } from 'react-router';
+
+class Home extends React.Component {
     render() {
         return (
-            <div id="content">
-                <h2>Learning React.js...</h2>
-                <ul>
-                    <li>Todo</li>
-                    <li>Timer</li>
-                </ul>
+            <div>
+                <h1>Trying out react.js</h1>
+            </div>
+        );
+    }
+}
+
+class App extends React.Component {
+    render() {
+        const routes = [
+            'Todo',
+            'Timer'
+        ];
+        return (
+            <div id="app">
+                <div id="nav">
+                    {routes.map((link, index) =>
+                        <Link key={index} to={`/${link.toLowerCase()}`} activeClassName="selected">{link}</Link>
+                    )}
+                </div>
+                <div id="content">
+                    {this.props.children}
+                </div>
             </div>
             );
     }
+}
+
+App.propTypes = {
+    children: React.PropTypes.element.isRequired
+};
+
+export default function() {
+    ReactDOM.render((
+    <Router history={browserHistory}>
+        <Route path="/" component={App}>
+            <IndexRoute component={Home}/>
+            <Route path="todo" component={Todo}/>
+            <Route path="Timer" component={Timer}/>
+        </Route>
+    </Router>
+    ), document.getElementById('main'));
 }
--- app/initialize.jsx
+++ app/initialize.jsx
@@ -1,11 +1,5 @@
-import ReactDOM from 'react-dom';
-import React from 'react';
-import App from 'components/App.jsx';
-import Timer from 'components/Timer.jsx';
-import Todo from 'components/Todo.jsx';
+import init from 'components/App.jsx';
 
 document.addEventListener('DOMContentLoaded', () => {
-    ReactDOM.render(<App />, document.querySelector('#app'));
-    ReactDOM.render(<Todo />, document.querySelector('#todo'));
-    ReactDOM.render(<Timer />, document.querySelector('#timer'));
+    init();
 });
