57fe4068919b0e3fecf7ca329e4a2eddfdc3347c
Douman <douman@gmx.se>
2016-12-19 10:51:24 +0300

    Finalize ToDo

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -8,7 +8,7 @@ class Item extends React.Component {
         return (
             <li>
                 <label onChange={this.props.handler} className={className}>
-                    <input type="checkbox"></input>
+                    <input defaultChecked={!data.isActive} type="checkbox"></input>
                     {data.text}
                 </label>
             </li>
@@ -24,15 +24,23 @@ Item.propTypes = {
 export default class Todo extends React.Component {
     constructor() {
         super();
+        this._key_idx = 0; //Simple unique ID of Item. Incremented on each submit.
         this.state = {
             todos: [{
                 text: "Add something here",
-                isActive: true
-            }]
+                isActive: true,
+                id: this._key_idx
+            }],
+            form: {
+                disabled: true,
+                inactive_num: 0
+            }
         };
 
         this.submit = this.submit.bind(this);
+        this.change = this.change.bind(this);
         this.toogleItem = this.toogleItem.bind(this);
+        this.delete_items = this.delete_items.bind(this);
     }
 
     /**
@@ -40,8 +48,37 @@ export default class Todo extends React.Component {
      */
     toogleItem(idx) {
         this.setState((prev) => {
+            const isActive = !prev.todos[idx].isActive;
+
+            if (!isActive) {
+                prev.form.inactive_num += 1;
+            }
+            else {
+                prev.form.inactive_num -= 1;
+            }
+
             prev.todos[idx].isActive = !prev.todos[idx].isActive;
             return prev;
+        });
+    }
+
+    /**
+     * Handles changes to input.
+     */
+    change(event) {
+        const disabled = !event.target.value;
+        this.setState((prev) => {
+            prev.form.disabled = disabled;
+            return prev;
+        });
+    }
+
+    delete_items() {
+        this.setState((prev) => {
+            prev.todos = prev.todos.filter((elem) => {
+                return elem.isActive;
+            });
+            prev.form.inactive_num = 0;
         });
     }
 
@@ -55,10 +92,13 @@ export default class Todo extends React.Component {
 
         if (!text) return;
 
+        this._key_idx += 1;
+
         this.setState((prev) => {
             prev.todos.push({
                 text: text,
-                isActive: true
+                isActive: true,
+                id: this._key_idx
             });
             return prev;
         });
@@ -67,15 +107,16 @@ export default class Todo extends React.Component {
     render() {
         return (
             <div id="content">
-                <h2>Todo:</h2>
+                <h1>ToDo</h1>
                 <ul>
                     {this.state.todos.map((todo, index) =>
-                        <Item handler={()=>this.toogleItem(index)} key={index} todo={todo}/>
+                        <Item handler={()=>this.toogleItem(index)} key={todo.id} todo={todo}/>
                     )}
                 </ul>
                 <form onSubmit={this.submit}>
-                    <input placeholder='Something toremember?'/>
-                    <input type="submit" value="Submit"/>
+                    <input onChange={this.change} placeholder='Something toremember?'/>
+                    <button disabled={this.state.form.disabled}>Submit</button>
+                    <button type='button' onClick={this.delete_items} disabled={!this.state.form.inactive_num}>X{this.state.form.inactive_num}</button>
                 </form>
             </div>
             );
